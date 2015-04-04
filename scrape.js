17e3e0ce029417c6032451546aa0e6287c8ae071
Brannon Dorsey <brannon@brannondorsey.com>
2015-04-04 17:36:31 -0500

    Now using unoffical API from http://www5.musatcha.com/musatcha/computers/wigleapi.htm

---
+++ scrape.js
@@ -0,0 +1,23 @@
+var wigle = require('./index.js');
+
+var client = wigle.createClient(
+    'brannon',
+    'wiglepasswosrd'
+);
+
+var parameters = {
+    longrange1: -87.627769,
+    longrange2: -87.624421,
+    latrange1: 41.879462,
+    latrange2: 41.882034,
+    simple: true,
+    variance: 0.010,
+    ssid: 'linksys'
+};
+
+client.query(parameters, function(err, result) {
+	  if (err) throw err;
+	  console.log('Timestamp:', result.timestamp);
+	  console.log('Number of networks found:', result.networks.length);
+	  // console.log('Networks:', result.networks);
+});
